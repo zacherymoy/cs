6ca3cb0dfe42d72e0e71a9963d25e72fdb6c1a32
zacherymoy <49412517+zacherymoy@users.noreply.github.com>
2020-07-21 17:30:31 -0500

    done

--- sprint1/sprint1/cityreader.py
+++ sprint1/sprint1/cityreader.py
@@ -1,6 +1,10 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -15,28 +19,25 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
+def cityreader():
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the 
+    # `cities` list
+    cities=[]
 
-import pandas as pd 
-df = pd.read_csv("https://raw.githubusercontent.com/LambdaSchool/Sprint-Challenge--Intro-Python/master/src/cityreader/cities.csv", 
-                 names=['city', 'state_name', 'county_name', 'lat', 'lng', 'population', 'density', 'timezone', 'zips']
-)
-df.head()
+    import csv
+    with open('cities.csv') as csvfile:
+        cityreader = csv.reader(csvfile)
+        next(cityreader)
+        for row in cityreader:
+          name, lat, lon = row[0], *row[3:5]
+          city = City(name, float(lat), float(lon))
+          cities.append(city)
+                
+    return cities
 
-class City:
-    def __init__(self, name, lat, lon):
-        self.name = name
-        self.lat = lat
-        self.lon = lon
-
-    def __str__(self):
-        return f'{self.name}, lat: {self.lat}, lon: {self.lon}'
-
-
-import csv
-cities = []
-
-# cities = cityreader(cities)
+cities = cityreader()
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
